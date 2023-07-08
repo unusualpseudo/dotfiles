72a4d56db545270296f175df2f391d48053e7c98
unusualpseudo <spik3spiegel@proton.me>
2023-07-08 22:52:53 +0200

    fix(fish): scripts tunning
    Signed-off-by: unusualpseudo <spik3spiegel@proton.me>

--- home/.chezmoiscripts/onchange_after/run_onchange_after_20-setup-completions.fish
+++ home/.chezmoiscripts/onchange_after/run_onchange_after_20-setup-completions.fish
@@ -8,37 +8,37 @@ if not test -d $__fish_config_dir/completions
 end
 
 if type -q chezmoi
-  echo "setting up chezmoi completion"
+  echo "Setting up chezmoi completion"
   chezmoi completion fish --output=$__fish_config_dir/completions/chezmoi.fish
 end
 
 if type -q flux
-  echo "setting up flux completion"
+  echo "Setting up flux completion"
   flux completion fish > $__fish_config_dir/completions/flux.fish
 end
 
 if type -q kustomize
-  echo "setting up kustomize completion"
+  echo "Setting up kustomize completion"
   kustomize completion fish > $__fish_config_dir/completions/kustomize.fish
 end
 
 if type -q kubectl
-  echo "setting up kubectl completion"
+  echo "Setting up kubectl completion"
   kubectl completion fish > $__fish_config_dir/completions/kubectl.fish
 end
 
 if type -q helm
-  echo "setting up helm completion"
+  echo "Setting up helm completion"
   helm completion fish > $__fish_config_dir/completions/helm.fish
 end
 
 if type -q k9s
-  echo "setting up k9s completion"
+  echo "Setting up k9s completion"
   k9s completion fish > $__fish_config_dir/completions/k9s.fish
 end
 
 if type -q task
-  echo "setting up task completion"
+  echo "Setting up task completion"
   curl --silent --show-error https://raw.githubusercontent.com/go-task/task/master/completion/fish/task.fish > $__fish_config_dir/completions/task.fish
 end
 
---
+++ home/dot_config/fish/custom_functions.d/mkcd.fish
@@ -0,0 +1,4 @@
+function mkcd --description 'mkdir and cd into it'
+    mkdir -p $argv
+    cd $argv
+  end
